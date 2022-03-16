85fa5b5e29c2d20d9a8ca33b37dcff4ec560ae7d
chunduri bhavana <frootichunduri@gmail.com>
2022-03-16 14:58:40 +0530

    index page changed

--- reactapp/src/Login.js
+++ reactapp/src/Login.js
@@ -2,6 +2,7 @@ import React,{useState} from 'react';
 import './Login.css';
 import { Link } from 'react-router-dom';
 const Login = () => {
+ 
 const[data,setData] = useState({
   email :'',
   password :'',
@@ -15,9 +16,9 @@ const submitHandler = e =>{
   console.log(data)
 }
 return(
-  
+
     <div className='container'>
-        
+      
     <center>
       <form onSubmit={submitHandler}>
       <label for="email"><h3>Enter Email Id</h3> :  
@@ -25,8 +26,8 @@ return(
         
         <label for="password"><h3>Enter Password</h3> : 
         <input type="password" id="password" name="password" value={password} onChange={changeHandler} /> <br /> </label>
+        <input type="submit" id="submitButton" name="login" value="Login" />
         
-        <input type="submit" id="loginButton"  name="login" value="Login" className='button' />
         <br />
         <p>
         New User/admin? <Link to='/signup'>sign up</Link>
