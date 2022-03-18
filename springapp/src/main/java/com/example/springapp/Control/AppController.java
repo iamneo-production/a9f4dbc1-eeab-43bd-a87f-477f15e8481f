0f82f03c2ff5a39c86314bea9d931aec92a48e75
dhanushsss <dhanush.sridhar01@gmail.com>
2022-03-18 18:09:36 +0530

    Fixed

--- springapp/src/main/java/com/example/springapp/Control/AppController.java
+++ springapp/src/main/java/com/example/springapp/Control/AppController.java
@@ -1,17 +1,15 @@
 package com.example.springapp.Control;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import com.example.springapp.Repository.FileRepository;
 import com.example.springapp.Service.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import com.example.springapp.Message.ResponseFile;
@@ -52,4 +50,5 @@ public class AppController {
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + fileDB.getName() + "\"")
                 .body(fileDB.getData());
     }
+
 }
